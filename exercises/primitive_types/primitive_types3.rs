90851b0b2c5edc80244df7cd8f2d49f1a9ffbfd0
bossm0n5t3r <bossm0n5t3r@gmail.com>
2020-09-27 02:07:02 +0900

    DONE

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -1,15 +1,16 @@
 // primitive_types3.rs
-// Create an array with at least 100 elements in it where the ??? is. 
+// Create an array with at least 100 elements in it where the ??? is.
 // Execute `rustlings hint primitive_types3` for hints!
 
-// I AM NOT DONE
+// I AM DONE
 
 fn main() {
-    let a = ???
+    let a = [1; 32];
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
     } else {
         println!("Meh, I eat arrays like that for breakfast.");
+        println!("{:?}", a);
     }
 }
