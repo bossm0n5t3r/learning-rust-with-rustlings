e3c19b0144c025475de7a3d102d9d7300ed2b703
bossm0n5t3r <bossm0n5t3r@gmail.com>
2020-09-26 23:03:19 +0900

    DONE

--- exercises/functions/functions1.rs
+++ exercises/functions/functions1.rs
@@ -1,8 +1,12 @@
 // functions1.rs
 // Make me compile! Execute `rustlings hint functions1` for hints :)
 
-// I AM NOT DONE
+// I AM DONE
 
 fn main() {
     call_me();
+}
+
+fn call_me() {
+    println!("hello, world!")
 }
