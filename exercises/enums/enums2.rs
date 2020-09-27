32e2aeb325caee27d044ddf9b1f9ffa7010b9852
bossm0n5t3r <bossm0n5t3r@gmail.com>
2020-09-28 03:19:30 +0900

    DONE

--- exercises/enums/enums2.rs
+++ exercises/enums/enums2.rs
@@ -1,11 +1,15 @@
 // enums2.rs
 // Make me compile! Execute `rustlings hint enums2` for hints!
 
-// I AM NOT DONE
+// I AM DONE
 
 #[derive(Debug)]
 enum Message {
     // TODO: define the different variants used below
+    Move { x: i32, y: i32 },
+    Echo(String),
+    ChangeColor(i32, i32, i32),
+    Quit,
 }
 
 impl Message {
@@ -16,10 +20,10 @@ impl Message {
 
 fn main() {
     let messages = [
-        Message::Move{ x: 10, y: 30 },
+        Message::Move { x: 10, y: 30 },
         Message::Echo(String::from("hello world")),
         Message::ChangeColor(200, 255, 255),
-        Message::Quit
+        Message::Quit,
     ];
 
     for message in &messages {
