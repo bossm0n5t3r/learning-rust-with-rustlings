9d78808d32933eba76aa991501df6deea7a05d6a
bossm0n5t3r <bossm0n5t3r@gmail.com>
2020-10-02 00:10:05 +0900

    DONE

--- exercises/clippy/clippy1.rs
+++ exercises/clippy/clippy1.rs
@@ -6,12 +6,13 @@
 // check clippy's suggestions from the output to solve the exercise.
 // Execute `rustlings hint clippy1` for hints :)
 
-// I AM NOT DONE
+// I AM DONE
 
 fn main() {
     let x = 1.2331f64;
     let y = 1.2332f64;
-    if y != x {
+    let error = f64::EPSILON;
+    if (y - x).abs() > error {
         println!("Success!");
     }
 }
