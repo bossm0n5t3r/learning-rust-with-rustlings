e582c41f62c9e3a5512552f4e54548a10f75c549
bossm0n5t3r <bossm0n5t3r@gmail.com>
2021-01-08 00:29:29 +0900

    DONE

--- exercises/collections/vec2.rs
+++ exercises/collections/vec2.rs
@@ -7,12 +7,15 @@
 // Execute the command `rustlings hint collections2` if you need
 // hints.
 
-// I AM NOT DONE
+// I AM DONE
+
+// https://doc.rust-lang.org/std/vec/struct.Vec.html#method.iter_mut
 
 fn vec_loop(mut v: Vec<i32>) -> Vec<i32> {
     for i in v.iter_mut() {
         // TODO: Fill this up so that each element in the Vec `v` is
         // multiplied by 2.
+        *i *= 2;
     }
 
     // At this point, `v` should be equal to [4, 8, 12, 16, 20].
