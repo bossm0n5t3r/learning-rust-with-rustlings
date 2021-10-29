809d8836871df41097225c59fcc26865420cafd5
bossm0n5t3r <bossm0n5t3r@gmail.com>
2021-10-29 21:00:00 +0900

    DONE
    - iterators4.rs
      - First Solution

--- exercises/standard_library_types/iterators4.rs
+++ exercises/standard_library_types/iterators4.rs
@@ -1,6 +1,6 @@
 // iterators4.rs
 
-// I AM NOT DONE
+// I AM DONE
 
 pub fn factorial(num: u64) -> u64 {
     // Complete this function to return the factorial of num
@@ -12,6 +12,11 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+
+    // First Solution
+    let mut result = 1;
+    (1..num + 1).for_each(|x| result *= x);
+    result
 }
 
 #[cfg(test)]
